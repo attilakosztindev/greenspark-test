c018e53d347cef1ffd11b9f17547ea7b5f7162d6
attilakosztin <attilakosztin.dev@gmail.com>
2024-12-15 21:08:53 +0100

    netlify config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,36 +1,36 @@
-import type { StorybookConfig } from '@storybook/vue3-vite'
-import { mergeConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import type { StorybookConfig } from "@storybook/vue3-vite"
+import { mergeConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import path from "path"
 
 const config: StorybookConfig = {
-  framework: '@storybook/vue3-vite',
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  framework: "@storybook/vue3-vite",
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y'
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y"
   ],
   core: {
-    builder: '@storybook/builder-vite',
+    builder: "@storybook/builder-vite",
     disableTelemetry: true
   },
   docs: {
     autodocs: true
   },
-  staticDirs: ['../public'],
+  staticDirs: ["../public"],
   async viteFinal(config) {
     return mergeConfig(config, {
       plugins: [vue()],
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '../src')
+          "@": path.resolve(__dirname, "../src")
         }
       },
       optimizeDeps: {
-        include: ['vue', 'vuetify']
+        include: ["vue", "vuetify"]
       },
-      base: process.env.STORYBOOK_BASE_URL || '/',
+      base: "/storybook/",
     })
   }
 }
